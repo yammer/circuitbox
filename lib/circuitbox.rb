a181077e8c8763eee9e1e9a05cae839047319e19
Matthew Shafer <matthewshafer@mac.com>
2019-03-26 14:32:38 -0700

    Change Circuitbox#circuit options hash to not be optional.
    Circuitbox circuits require an array of exceptions to be passed when creating the circuit.

--- lib/circuitbox.rb
+++ lib/circuitbox.rb
@@ -12,7 +12,7 @@ class Circuitbox
   class << self
     include Configuration
 
-    def circuit(service_name, options = {})
+    def circuit(service_name, options)
       circuit = (cached_circuits[service_name] ||= CircuitBreaker.new(service_name, options))
 
       return circuit unless block_given?
