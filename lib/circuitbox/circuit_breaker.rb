568a40c5d49e3ea3efa2f2caa096279ace5a24f1
Matthew Shafer <matthewshafer@mac.com>
2021-02-06 21:30:48 -0800

    Initial work on improving code documentation

--- lib/circuitbox/circuit_breaker.rb
+++ lib/circuitbox/circuit_breaker.rb
@@ -16,16 +16,20 @@ class Circuitbox
       time_window: 60
     }.freeze
 
+    # Initialize a CircuitBreaker
     #
-    # Configuration options
+    # @param service [String, Symbol] Name of the circuit for notifications and metrics store
+    # @param options [Hash] Options to create the circuit with
+    # @option options [Integer] :time_window (60) Interval of time, in seconds, used to calculate the error_rate
+    # @option options [Integer, Proc] :sleep_window (90) Seconds for the circuit to stay open when tripped
+    # @option options [Integer, Proc] :volume_threshold (5) Number of requests before error rate is first calculated
+    # @option options [Integer, Proc] :error_threshold (50) Percentage of failed requests needed to trip the circuit
+    # @option options [Array] :exceptions The exceptions that should be monitored and counted as failures
+    # @option options [Circuitbox::MemoryStore, Moneta] :cache (Circuitbox.default_circuit_store) Class to store circuit open/close statistics
+    # @option options [Logger] :logger (Circuitbox.default_logger) Logging class for circuit messages
+    # @option options [Object] :notifier (Circuitbox.default_notifier) Class notifications are sent to
     #
-    # `sleep_window`      - seconds to sleep the circuit
-    # `volume_threshold`  - number of requests before error rate calculation occurs
-    # `error_threshold`   - percentage of failed requests needed to trip circuit
-    # `exceptions`        - exceptions that count as failures
-    # `time_window`       - interval of time used to calculate error_rate (in seconds) - default is 60s
-    # `logger`            - Logger to use - defaults to Rails.logger if defined, otherwise STDOUT
-    #
+    # @raise [ArgumentError] If the exceptions option is not an Array
     def initialize(service, options = {})
       @service = service.to_s
       @circuit_options = DEFAULTS.merge(options)
@@ -51,6 +55,21 @@ class Circuitbox
       value.is_a?(Proc) ? value.call : value
     end
 
+    # Run the circuit with the given block.
+    # If the circuit is closed or half_open the block will run.
+    # If the circuit is open the block will not be run.
+    #
+    # @param circuitbox_exceptions [Boolean] If exceptions should be raised when the circuit is open
+    #   or when a watched exception is raised from the block
+    # @yield Block to run if circuit is not open
+    #
+    # @raise [Circuitbox::OpenCircuitError] If the circuit is open and circuitbox_exceptions is true
+    # @raise [Circuitbox::ServiceFailureError] If a tracked exception is raised from the block and circuitbox_exceptions is true
+    #
+    # @return [Object] The result from the block
+    # @return [Nil] If the circuit is open and circuitbox_exceptions is false
+    #   In cases where an exception that circuitbox is watching is raised from either a notifier
+    #   or from a custom circuit cache nil can be returned even though the block ran successfully
     def run(circuitbox_exceptions: true, &block)
       if open?
         skipped!
@@ -64,7 +83,7 @@ class Circuitbox
           success!
         rescue *exceptions => e
           # Other stores could raise an exception that circuitbox is asked to watch.
-          # setting to nil keeps the same behavior as the previous defination of run.
+          # setting to nil keeps the same behavior as the previous definition of run.
           response = nil
           failure!
           raise Circuitbox::ServiceFailureError.new(service, e) if circuitbox_exceptions
@@ -74,10 +93,16 @@ class Circuitbox
       response
     end
 
+    # Check if the circuit is open
+    #
+    # @return [Boolean] True if circuit is open, False if closed
     def open?
       circuit_store.key?(open_storage_key)
     end
 
+    # Calculates the current error rate of the circuit
+    #
+    # @return [Float] Error Rate
     def error_rate(failures = failure_count, success = success_count)
       all_count = failures + success
       return 0.0 unless all_count.positive?
@@ -85,14 +110,25 @@ class Circuitbox
       (failures / all_count.to_f) * 100
     end
 
+    # Number of Failures the circuit has encountered in the current time window
+    #
+    # @return [Integer] Number of failures
     def failure_count
       circuit_store.load(stat_storage_key('failure'), raw: true).to_i
     end
 
+    # Number of successes the circuit has encountered in the current time window
+    #
+    # @return [Integer] Number of successes
     def success_count
       circuit_store.load(stat_storage_key('success'), raw: true).to_i
     end
 
+    # If the circuit is open the key indicating that the circuit is open
+    # On the next call to run the circuit would run as if it were in the half open state
+    #
+    # This does not reset any of the circuit success/failure state so future failures
+    # in the same time window may cause the circuit to open sooner
     def try_close_next_time
       circuit_store.delete(open_storage_key)
     end
