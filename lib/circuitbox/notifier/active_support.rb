dd56c6b15eec34f3e64cfc1fd26287f3b33aa603
Matthew Shafer <matthewshafer@mac.com>
2022-01-19 01:28:26 -0800

    Rename ActiveSupport::Notifications events to follow rails conventions The rails convention is event.library

--- lib/circuitbox/notifier/active_support.rb
+++ lib/circuitbox/notifier/active_support.rb
@@ -4,15 +4,15 @@ class Circuitbox
   class Notifier
     class ActiveSupport
       def notify(circuit_name, event)
-        ::ActiveSupport::Notifications.instrument("circuit_#{event}", circuit: circuit_name)
+        ::ActiveSupport::Notifications.instrument("#{event}.circuitbox", circuit: circuit_name)
       end
 
       def notify_warning(circuit_name, message)
-        ::ActiveSupport::Notifications.instrument('circuit_warning', circuit: circuit_name, message: message)
+        ::ActiveSupport::Notifications.instrument('warning.circuitbox', circuit: circuit_name, message: message)
       end
 
       def notify_run(circuit_name, &block)
-        ::ActiveSupport::Notifications.instrument('circuit_run', circuit: circuit_name, &block)
+        ::ActiveSupport::Notifications.instrument('run.circuitbox', circuit: circuit_name, &block)
       end
     end
   end
